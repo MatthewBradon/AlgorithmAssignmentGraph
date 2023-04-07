8c2a5f0a08ce421e26470bbd911abb2846f702ef
Matthew Bradon <matthewbradon@gmail.com>
2023-04-07 21:38:41 +0100

    added dijkstra algo, fixed siftdown siftup and prim

--- GraphLists.java
+++ GraphLists.java
@@ -55,98 +55,121 @@ class GraphQueue {
     }
 }
 
-class Heap {
-    private int[] a; // heap array
-    private int[] hPos; // hPos[h[k]] == k
-    private int[] dist; // dist[v] = priority of v
+class Heap
+{
+    private int[] a;       // heap array
+    private int[] hPos;    // hPos[a[k]] == k
+    private int[] dist;    // dist[v] = priority of v
+    private int MAX;
 
-    private int N; // heap size
+    private int N;         // heap size
 
     // The heap constructor gets passed from the Graph:
-    // 1. maximum heap size
-    // 2. reference to the dist[] array
-    // 3. reference to the hPos[] array
-    public Heap(int maxSize, int[] _dist, int[] _hPos) {
+    //    1. maximum heap size
+    //    2. reference to the dist[] array
+    //    3. reference to the hPos[] array
+    public Heap(int maxSize, int[] _dist, int[] _hPos) 
+    {
         N = 0;
+        MAX = maxSize;
         a = new int[maxSize + 1];
         dist = _dist;
         hPos = _hPos;
     }
 
-    public boolean isEmpty() {
+
+    public boolean isEmpty() 
+    {
         return N == 0;
     }
 
-    public void print() {
-        for (int i = 1; i <= N; i++) {
-            System.out.print(a[i] + " ");
-        }
-        System.out.println();
-    }
 
-    public void siftUp(int k) {
+    public void siftUp( int k) 
+    {
         int v = a[k];
 
-        System.out.println("SiftUp: Sifting " + v + " up");
-        System.out.println("Before siftUp: ");
-        this.print();
-        while (dist[v] < dist[k / 2]) {
-            a[k] = a[k / 2];
-            hPos[k] = k;
-            k = k / 2;
+        a[0] = 0;
+        dist[0] = Integer.MIN_VALUE;
 
+        //vertex using dist moved up heap
+        while(dist[v] < dist[a[k/2]]){
+
+
+            a[k] = a[k/2]; //parent vertex is assigned pos of child vertex
+
+            hPos[a[k]] = k;//hpos modified for siftup
+
+            k = k/2;// index of child assigned last parent to continue siftup
         }
-        a[k] = v;
-        hPos[v] = k;
-        System.out.println("After siftUp: ");
-        this.print();
+
+        a[k] = v;//resting pos of vertex assigned to heap
+
+        hPos[v] = k;//index of resting pos of vertex updated in hpos
+
+        //display hpos array
+       /* System.out.println("\nThe following is the hpos array after siftup: \n");
+
+        for(int i = 0; i < MAX; i ++){
+
+            System.out.println("%d", hPos[i]);
+        }
+
+        System.out.println("\n Following is heap array after siftup: \n");
+
+        for (int i = 0; i < MAX; i ++ ){
+
+            System.out.println("%d" , a[i]);
+
+        }*/
     }
 
-    public void siftDown(int k) {
+
+    //removing the vertex at top of heap
+    //passed the index of the smallest value in heap
+    //siftdown resizes and resorts heap
+
+    public void siftDown( int k) 
+    {
         int v, j;
-        v = a[k];
-        j = 2 * k;
-        System.out.println("SiftDown Sifting " + v + " down");
-        System.out.println("Before siftDown: ");
-        this.print();
-        while (j <= N - 1) {
 
-            if (dist[a[j]] < dist[a[j + 1]] && j < N - 1) {
-                j++;
-            }
-            if (dist[v] <= dist[a[j]]) {
-                break;
-            }
+        v = a[k];  
 
-            a[j] = a[k];
+        while(k <= N/2){
 
-            k = j;
             j = 2 * k;
 
+            if(j < N && dist[a[j]] > dist[a[j + 1]]) ++j; //if node is > left increment j child
+
+            if(dist[v] <= dist[a[j]]) break;//if sizeof parent vertex is less than child stop.
+
+            a[k] = a[j];//if parent is greater than child then child assigned parent pos
+
+            hPos[a[k]] = k;//update new pos of last child
+
+            k = j;//assign vertex new pos
         }
-        a[k] = v;
-        hPos[v] = k;
-        System.out.println("After siftDown: ");
-        this.print();
+        a[k] = v;//assign rest place of vertex to heap
+        hPos[v] = k;//update pos of the vertex in hpos array
     }
 
-    public void insert(int x) {
-        System.out.println("Inserting " + x + " current heap:");
-        this.print();
-        a[++N] = x;
-        siftUp(N);
+
+    public void insert( int x) 
+    {
+        a[++N] = x;//assign new vertex to end of heap
+        siftUp( N);//pass index at end of heap to siftup
     }
 
-    public int remove() {
-        System.out.println("Removing " + a[1] + " current heap:");
-        this.print();
+
+    public int remove() 
+    {   
         int v = a[1];
         hPos[v] = 0; // v is no longer in heap
-        a[N + 1] = 0; // put null node into empty spot
-        a[1] = a[N--];
-        siftDown(1);
+        a[N+1] = 0;  // put null node into empty spot
 
-        return v;
+        a[1] = a[N--];//last node of heap moved to top
+        siftDown(1);//pass index at top to siftdown
+
+        return v;//return vertex at top of heap
     }
 
 }
@@ -176,7 +199,7 @@ class Graph {
     private int V, E;
     private Node[] adj;
     private Node z;
-    private int[] mst;
+    private int[] mst, spt;
 
     // used for traversing graph
     private int[] visited;
@@ -219,28 +242,17 @@ class Graph {
             v = Integer.parseInt(parts[1]);
             wgt = Integer.parseInt(parts[2]);
 
-            System.out.println("Edge " + toChar(u) + "--(" + wgt + ")--" + toChar(v));
+            System.out.println("" + toChar(u) + "-(" + wgt + ")-" + toChar(v));
 
             // write code to put edge into adjacency matrix
-            t = adj[u];
-            if (t == z) {
-                adj[u] = new Node(v, wgt, z);
-            } else {
-                while (t.next != z) {
-                    t = t.next;
-                }
-                t.next = new Node(v, wgt, z);
-            }
+            t = new Node(v, wgt, adj[u]);
+            adj[u] = t;
 
-            t = adj[v];
-            if (t == z) {
-                adj[v] = new Node(u, wgt, z);
-            } else {
-                while (t.next != z) {
-                    t = t.next;
-                }
-                t.next = new Node(u, wgt, z);
-            }
+            t = new Node(u, wgt, adj[v]);
+            adj[v] = t;
+            
+
+
 
         }
     }
@@ -271,7 +283,6 @@ class Graph {
     }
 
     public void MST_Prim(int s) {
-        int INFINITY = 9999999;
         int v, u;
         int wgt, wgt_sum = 0;
         int[] dist, parent, hPos;
@@ -282,26 +293,24 @@ class Graph {
         hPos = new int[V + 1];
         parent = new int[V + 1];
 
-        Heap h = new Heap(V, dist, hPos);
-        h.insert(s);
-
-        for (v = 1; v <= V; v++) {
-            if (v != s) {
-                dist[v] = INFINITY;
-                parent[v] = 0;
-                hPos[v] = 0;
-            }
+        for (v = 0; v <= V; v++) {
+            dist[v] = Integer.MAX_VALUE;
+            parent[v] = 0;
+            hPos[v] = 0;
         }
 
         dist[s] = 0;
-        parent[s] = 0;
+
+        Heap h = new Heap(V, dist, hPos);
+        h.insert(s);
+
         while (!h.isEmpty()) {
 
             v = h.remove();
-            t = adj[v];
+            wgt_sum += dist[v];
+            dist[v] = -dist[v];
 
             for (t = adj[v]; t != z; t = t.next) {
-                // System.out.println("v = " + v + " t = " + t);
                 u = t.vert;
                 wgt = t.wgt;
                 if (wgt < dist[u]) {
@@ -321,10 +330,6 @@ class Graph {
 
         for (v = 0; v <= V; v++) {
             System.out.println("v = " + v + " dist = " + dist[v] + " parent = " + parent[v]);
-            if (parent[v] > 0) {
-                wgt_sum += dist[v];
-            }
-
         }
         System.out.print("\n\nWeight of MST = " + wgt_sum + "\n");
 
@@ -339,7 +344,56 @@ class Graph {
     }
 
     public void SPT_Dijkstra(int s) {
+        int v, u;
+        int wgt;
+        int[] dist, parent, hPos;
+        Node t;
+        dist = new int[V + 1];
+        hPos = new int[V + 1];
+        parent = new int[V + 1];
 
+        for (v = 1; v <= V; v++) {
+            dist[v] = Integer.MAX_VALUE;
+            parent[v] = 0;
+            hPos[v] = 0;
+        }
+
+        
+
+        dist[s] = 0;
+        v = s;
+
+        Heap h = new Heap(V, dist, hPos);
+        h.insert(s);
+        while (!h.isEmpty()){
+            for(t = adj[v]; t != z; t = t.next){
+                u = t.vert;
+                System.out.println("u = " + u + " v = " + v);
+                wgt = t.wgt;
+
+                if (dist[u] > dist[v] + wgt){
+
+                    dist[u] = dist[v] + wgt;
+                    parent[u] = v;
+                    
+                    if (hPos[u] == 0){
+                        h.insert(u);
+                    } else {
+                        System.out.println("test sift up U: " + u + " hPos[u]: " + hPos[u]);
+
+                        h.siftUp(hPos[u]);
+                    }
+                    
+                }
+            }
+            v = h.remove();
+        }
+        
+        for (v = 0; v <= V; v++) {
+            System.out.println("v = " + toChar(v) + " parent = " + toChar(parent[v]) + " dist = " + dist[v]);
+
+        }
+        spt = parent;
     }
 
     public void DF(int s) {
@@ -404,11 +458,11 @@ public class GraphLists {
 
         g.display();
 
-        g.DF(s);
+        //g.DF(s);
         System.out.println();
-        g.breadthFirst(s);
+        //g.breadthFirst(s);
         System.out.println();
-        g.MST_Prim(s);
-        // g.SPT_Dijkstra(s);
+        //g.MST_Prim(s);
+        g.SPT_Dijkstra(s);
     }
 }
